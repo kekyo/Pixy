22452f6ac33574ea01936630fba4d11d002fd98b
Kouji Matsui <k@kekyo.net>
2023-11-22 00:13:59 +0900

    Succeeded 1word read-write access.

--- Pixy-mega128-0-sdram-hat/SdramHat/src/main.cpp
+++ Pixy-mega128-0-sdram-hat/SdramHat/src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <avr/io.h>
+#include <util/delay.h>
 
 // Pin declaration for Pixy-mega128-sdram-hat
 
@@ -49,17 +50,63 @@
 #define SDRAM_A11 PIN_PC3
 
 static void set_DQ_mode(bool isOutput) {
-  PORTF = 0x00;
-  PORTA = 0x00;
   if (isOutput) {
     DDRF = 0xff;   // PF: DQ0 - DQ7
     DDRA = 0xff;   // PA: DQ8 - DQ15
   } else {
     DDRF = 0x00;   // PF: DQ0 - DQ7
     DDRA = 0x00;   // PA: DQ8 - DQ15
+    PORTF = 0x00;
+    PORTA = 0x00;
   }
 }
 
+static void output_pulse(
+  int pin, int periodMicroseconds, int count) {
+  unsigned long next = micros() + periodMicroseconds;
+  unsigned long current;
+  while (count > 0) {
+    digitalWrite(pin, HIGH);
+    while (true) {
+      current = micros();
+      if (current >= next) {
+        break;
+      }
+    }
+    next += periodMicroseconds;
+
+    digitalWrite(pin, LOW);
+    while (true) {
+      current = micros();
+      if (current >= next) {
+        break;
+      }
+    }
+    next += periodMicroseconds;
+
+    count--;
+  }
+}
+
+static void output_Address(uint16_t address) {
+  PORTD = (uint8_t)address;
+  PORTC = (PORTC & 0xc0) | ((address >> 8) & 0x3f);
+}
+
+static void output_DQ(uint16_t data) {
+  PORTA = (uint8_t)data;
+  PORTF = (uint8_t)(data >> 8);
+  set_DQ_mode(true);
+}
+
+static uint16_t input_DQ() {
+  set_DQ_mode(false);
+  delayMicroseconds(1);
+  return PINA | (((uint16_t)PINF) << 8);
+}
+
+////////////////////////////////////////////////////////////
+  
 void setup() {
   Serial.begin(115200);
   Serial.println("setup()");
@@ -71,6 +118,7 @@ void setup() {
   PORTC = 0x00;
   DDRD = 0xff;   // PD: A0 - A7
   DDRC = 0xff;   // PC: A8 - A11, BA0, BA1, CKE, CLK
+  digitalWrite(SDRAM_CKE, HIGH);
 
   //DDRB = 0xf0;   // PB: WE, CAS, RAS, CS
   digitalWrite(SDRAM_WE, HIGH);
@@ -87,8 +135,165 @@ void setup() {
   digitalWrite(SDRAM_UDQM, HIGH);
   pinMode(SDRAM_LDQM, OUTPUT);
   pinMode(SDRAM_UDQM, OUTPUT);
+
+  _delay_us(100);
+
+  ///////////////////////////////
+
+  // PALL:
+  digitalWrite(SDRAM_CKE, HIGH);
+  digitalWrite(SDRAM_CS, LOW);
+  digitalWrite(SDRAM_RAS, LOW);
+  digitalWrite(SDRAM_CAS, HIGH);
+  digitalWrite(SDRAM_WE, LOW);
+  digitalWrite(SDRAM_LDQM, HIGH);
+  digitalWrite(SDRAM_UDQM, HIGH);
+  digitalWrite(SDRAM_BA0, LOW);
+  digitalWrite(SDRAM_BA1, LOW);
+  output_Address(0x400);   // A10=HIGH
+  output_pulse(SDRAM_CLK, 10, 10);   // REAL specific average refresh rate: 15.6us
+  digitalWrite(SDRAM_A10, LOW);
+
+  // REF:
+  digitalWrite(SDRAM_CKE, HIGH);
+  digitalWrite(SDRAM_CS, LOW);
+  digitalWrite(SDRAM_RAS, LOW);
+  digitalWrite(SDRAM_CAS, LOW);
+  digitalWrite(SDRAM_WE, HIGH);
+  digitalWrite(SDRAM_LDQM, HIGH);
+  digitalWrite(SDRAM_UDQM, HIGH);
+  output_Address(0x000);
+  output_pulse(SDRAM_CLK, 10, 30);    // Specific count >=8
+
+  // MRS:
+  digitalWrite(SDRAM_CKE, HIGH);
+  digitalWrite(SDRAM_CS, LOW);
+  digitalWrite(SDRAM_RAS, LOW);
+  digitalWrite(SDRAM_CAS, LOW);
+  digitalWrite(SDRAM_WE, LOW);
+  digitalWrite(SDRAM_LDQM, HIGH);
+  digitalWrite(SDRAM_UDQM, HIGH);
+  output_Address(0x220);    // MRS: BL=1word, BT=seq, LMODE=2cycle, OPCODE=BRSW
+  output_pulse(SDRAM_CLK, 10, 1);
+  
+  // IDLE:
+  digitalWrite(SDRAM_CKE, HIGH);
+  digitalWrite(SDRAM_CS, HIGH);
+  digitalWrite(SDRAM_RAS, HIGH);
+  digitalWrite(SDRAM_CAS, HIGH);
+  digitalWrite(SDRAM_WE, HIGH);
+  digitalWrite(SDRAM_LDQM, HIGH);
+  digitalWrite(SDRAM_UDQM, HIGH);
+  output_Address(0x000);
+  output_pulse(SDRAM_CLK, 10, 1);
 }
 
-void loop() {
+////////////////////////////////////////////////////////////
 
+uint16_t index = 0;
+
+void loop() {
+  // RAS:
+  digitalWrite(SDRAM_CKE, HIGH);
+  digitalWrite(SDRAM_CS, LOW);
+  digitalWrite(SDRAM_RAS, LOW);
+  digitalWrite(SDRAM_CAS, HIGH);
+  digitalWrite(SDRAM_WE, HIGH);
+  digitalWrite(SDRAM_LDQM, HIGH);
+  digitalWrite(SDRAM_UDQM, HIGH);
+  output_Address(0x000);
+  output_pulse(SDRAM_CLK, 10, 2);
+
+  // WRITE CAS:
+  digitalWrite(SDRAM_CKE, HIGH);
+  digitalWrite(SDRAM_CS, LOW);
+  digitalWrite(SDRAM_RAS, HIGH);
+  digitalWrite(SDRAM_CAS, LOW);
+  digitalWrite(SDRAM_WE, LOW);
+  digitalWrite(SDRAM_LDQM, LOW);
+  digitalWrite(SDRAM_UDQM, LOW);
+  output_Address(0x000);
+  output_DQ(index++);
+  output_pulse(SDRAM_CLK, 10, 2);
+
+  set_DQ_mode(false);
+
+  ///////////////////////////////////////
+
+  // IDLE:
+  digitalWrite(SDRAM_CKE, HIGH);
+  digitalWrite(SDRAM_CS, HIGH);
+  digitalWrite(SDRAM_RAS, HIGH);
+  digitalWrite(SDRAM_CAS, HIGH);
+  digitalWrite(SDRAM_WE, HIGH);
+  digitalWrite(SDRAM_LDQM, HIGH);
+  digitalWrite(SDRAM_UDQM, HIGH);
+  output_Address(0x000);
+  output_pulse(SDRAM_CLK, 10, 1);
+
+  // REF:
+  digitalWrite(SDRAM_CKE, HIGH);
+  digitalWrite(SDRAM_CS, LOW);
+  digitalWrite(SDRAM_RAS, LOW);
+  digitalWrite(SDRAM_CAS, LOW);
+  digitalWrite(SDRAM_WE, HIGH);
+  digitalWrite(SDRAM_LDQM, HIGH);
+  digitalWrite(SDRAM_UDQM, HIGH);
+  output_Address(0x000);
+  //output_pulse(SDRAM_CLK, 10, 8192);
+  output_pulse(SDRAM_CLK, 10, 100);
+
+  ///////////////////////////////////////
+
+  // RAS:
+  digitalWrite(SDRAM_CKE, HIGH);
+  digitalWrite(SDRAM_CS, LOW);
+  digitalWrite(SDRAM_RAS, LOW);
+  digitalWrite(SDRAM_CAS, HIGH);
+  digitalWrite(SDRAM_WE, HIGH);
+  digitalWrite(SDRAM_LDQM, HIGH);
+  digitalWrite(SDRAM_UDQM, HIGH);
+  output_Address(0x000);
+  output_pulse(SDRAM_CLK, 10, 2);
+
+  // READ CAS:
+  digitalWrite(SDRAM_CKE, HIGH);
+  digitalWrite(SDRAM_CS, LOW);
+  digitalWrite(SDRAM_RAS, HIGH);
+  digitalWrite(SDRAM_CAS, LOW);
+  digitalWrite(SDRAM_WE, HIGH);
+  digitalWrite(SDRAM_LDQM, HIGH);
+  digitalWrite(SDRAM_UDQM, HIGH);
+  output_Address(0x000);
+  output_pulse(SDRAM_CLK, 10, 2);
+
+  // READ [1]:
+  digitalWrite(SDRAM_CKE, HIGH);
+  digitalWrite(SDRAM_CS, LOW);
+  digitalWrite(SDRAM_RAS, HIGH);
+  digitalWrite(SDRAM_CAS, LOW);
+  digitalWrite(SDRAM_WE, HIGH);
+  digitalWrite(SDRAM_LDQM, HIGH);
+  digitalWrite(SDRAM_UDQM, HIGH);
+  output_Address(0x000);
+
+  digitalWrite(SDRAM_CLK, HIGH);
+  uint16_t r = input_DQ();
+  digitalWrite(SDRAM_CLK, LOW);
+
+  Serial.print("r=");
+  Serial.println(r, HEX);
+
+  ///////////////////////////////////////
+
+  // IDLE:
+  digitalWrite(SDRAM_CKE, HIGH);
+  digitalWrite(SDRAM_CS, HIGH);
+  digitalWrite(SDRAM_RAS, HIGH);
+  digitalWrite(SDRAM_CAS, HIGH);
+  digitalWrite(SDRAM_WE, HIGH);
+  digitalWrite(SDRAM_LDQM, HIGH);
+  digitalWrite(SDRAM_UDQM, HIGH);
+  output_Address(0x000);
+  output_pulse(SDRAM_CLK, 10, 1);
 }
