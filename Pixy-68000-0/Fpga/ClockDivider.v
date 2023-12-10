31174c63479cdaf839674a987cf03240bee67b53
Kouji Matsui <k@kekyo.net>
2023-12-10 19:36:51 +0900

    Updated readme.

--- Pixy-68000-0/Fpga/ClockDivider.v
+++ Pixy-68000-0/Fpga/ClockDivider.v
@@ -5,24 +5,24 @@ module ClockDivider(
 /////////////////////////////////////////////
 
 // CLK_IN=40MHz  (25ns)
-//always @ (posedge CLK_IN) begin
-//	CLK_OUT <= ~CLK_OUT;  // 50ns/20MHz
-//end
+always @ (posedge CLK_IN) begin
+	CLK_OUT <= ~CLK_OUT;  // 50ns/20MHz
+end
 
 /////////////////////////////////////////////
 
-reg [7:0] COUNT;
+//reg [7:0] COUNT;
 
 // For slower, testing purpose.
 // CLK_IN=40MHz  (25ns)
-always @ (posedge CLK_IN) begin
-    if (COUNT == 'd39) begin   // 2us/0.5MHz
+//always @ (posedge CLK_IN) begin
+//    if (COUNT == 'd39) begin   // 2us/0.5MHz
     //if (COUNT == 'd199) begin   // 10us/100kHz
-        CLK_OUT <= ~CLK_OUT;
-        COUNT <= 0;
-    end else begin
-        COUNT <= COUNT + 9'd1;
-    end
-end
+//        CLK_OUT <= ~CLK_OUT;
+//        COUNT <= 0;
+//    end else begin
+//        COUNT <= COUNT + 9'd1;
+//    end
+//end
 
 endmodule
